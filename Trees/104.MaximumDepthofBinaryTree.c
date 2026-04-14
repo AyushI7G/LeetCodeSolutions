6e090c29e49dfb029c1a18785c993dd4d4108ec5
Ayushi Gupta <ayushigupta1187@gmail.com>
2026-04-14 20:17:18 +0530

    Update 104.MaximumDepthofBinaryTree.c

--- Trees/104.MaximumDepthofBinaryTree.c
+++ Trees/104.MaximumDepthofBinaryTree.c
@@ -1,1 +1,22 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root) {
+    int depth = 0;
+    if(root == NULL){
+        return depth; 
+    }
+    int leftdepth = maxDepth(root->left);
+    int rightdepth = maxDepth(root->right);
+    if(leftdepth > rightdepth){
+        return 1 + leftdepth;
+    }
+    else{
+        return 1 + rightdepth;
+    }
+}
