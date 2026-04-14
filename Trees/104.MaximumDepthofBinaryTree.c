578b098ae229ba810b6a08b40f251daab0a23fd8
Ayushi Gupta <ayushigupta1187@gmail.com>
2026-04-14 20:23:13 +0530

    Update 104.MaximumDepthofBinaryTree.c

--- Trees/104.MaximumDepthofBinaryTree.c
+++ Trees/104.MaximumDepthofBinaryTree.c
@@ -20,22 +20,3 @@ int maxDepth(struct TreeNode* root) {
         return 1 + rightdepth;
     }
 }
-
-
-
-
-
-
-
-f hkahfk ahfkja 
-afdhka k fak ajg aja 
-ufe hf fekj kfsf kfehsk 
-aoif la flakjf lajf la
-alfj alkjf lajfl aflka 
-aksfalk fjla jfla jfls
-akjd flka jfkd fafa 
-alf jak fjldakj fla
-asf jlakjf kajfl 
-alf jalkf lakjfl fl af
-af jaf jlf akjfl ajfal 
-akfj alkfj lakjfl ajflk 
