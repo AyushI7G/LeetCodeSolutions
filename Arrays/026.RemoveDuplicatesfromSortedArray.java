904ef9c4374a0eecd99863967bd5c9d510ca1df4
Ayushi Gupta <ayushigupta1187@gmail.com>
2026-07-12 02:27:24 +0530

    Update 026.RemoveDuplicatesfromSortedArray.java

--- Arrays/026.RemoveDuplicatesfromSortedArray.java
+++ Arrays/026.RemoveDuplicatesfromSortedArray.java
@@ -1,5 +1,18 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for(int j = 1; j < nums.length; j++){
+            if(nums[j] != nums[i]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+}
+/*
+class Solution {
+    public int removeDuplicates(int[] nums) {
         for(int j = 0; j <nums.length-1; j++){
             if(nums[j] == nums[j+1]){
                 nums[j] = 101;
@@ -15,3 +28,4 @@ class Solution {
         return k;
     }
 }
+*/
