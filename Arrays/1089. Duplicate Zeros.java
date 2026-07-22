254c69747188dd58b087195541ad0fbe4347dcd0
Ayushi Gupta <ayushigupta1187@gmail.com>
2026-07-23 03:28:05 +0530

    Update 1089. Duplicate Zeros.java

--- Arrays/1089. Duplicate Zeros.java
+++ Arrays/1089. Duplicate Zeros.java
@@ -1,5 +1,26 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
+        int [] res = new int [arr.length];
+        int j = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[j] == 0 && i != arr.length-1){
+                res[i] = 0;
+                i++;
+                res[i] = 0;
+            }
+            else{
+                res[i] = arr[j];
+            }
+            j++;
+        }
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = res[i];
+        }S
+    }
+}
+/*
+class Solution {
+    public void duplicateZeros(int[] arr) {
         for(int i = 0; i < arr.length-1; i++){
             int j = arr.length-1;
             if(arr[i] == 0){
@@ -12,3 +33,4 @@ class Solution {
         }
     }
 }
+*/
